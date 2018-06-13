c59a8815f4d2340c7e5336abc237bfdf22873487
Kirsten Turner <TurnerK@oceania.cshare.net>
2018-06-13 14:14:38 +1000

    Module3

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion';
+
+
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-
+import QuizQuestionButton from './QuizQuestionButton';
 class QuizQuestion extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <main>
-           {/* <pre>{this.props.quiz_question}</pre>  */}
+        
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            <li>{this.props.quiz_question.answer_options[0]}</li>
+              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
           </ul>
         </section>
       </main>
